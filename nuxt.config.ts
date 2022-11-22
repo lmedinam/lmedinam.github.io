d223d5210e192b1badbb3f55845d04074637c532
Luis Medina <git@lmedinam.es>
2022-11-22 11:53:44 -0400

    Add favicon and meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss']
+    modules: ['@nuxtjs/tailwindcss'],
+    app: {
+        head: {
+            title: 'Luis Medina | Desarrollador Full-Stack',
+            meta: [
+                { name: 'title', content: 'Luis Medina | Desarrollador Full-Stack' },
+                { name: 'description', content: 'Con más de 5 años de experiencia, construyo sistemas sólidos y escalables con una excelente experiencia de usuario.' }
+            ],
+            link: [
+                { rel: 'icon', href: '/favicon.png' }
+            ]
+        }
+    }
 })
